f309e6118b070534a3ce39c50f5e5700890dc292
Nicolás Meza <ncmeza.234@gmail.com>
2022-11-09 23:58:21 -0300

    deleted unused chart function

--- views/js/functions.js
+++ views/js/functions.js
@@ -2,27 +2,33 @@ let is_collapsed = false
 //click events
 $('.side-button-collapse').on("click", collapseSideBar)
 
-//functions
+//functions 
 function collapseSideBar() {
     console.log(is_collapsed)
     if (!is_collapsed) {
         $('.side-bar').css('width', '100px')
         hideOrShowElements(is_collapsed)
         is_collapsed = true
-    }
-    else if (is_collapsed) {
+    }   else if (is_collapsed) {
         $('.side-bar').css('width', '500px')
         hideOrShowElements(is_collapsed)
         is_collapsed = false
     }
+    //$('#chart-container').width(getWidth())
 }
 
 function hideOrShowElements(is_collapsed) {
     if (!is_collapsed) {
         $('.display-pill-wrapper').hide()
+        $('.warm').hide()
         $('.main-icon').hide()
     } else if (is_collapsed) {
         $('.display-pill-wrapper').show()
+        $('.warm').show()
         $('.main-icon').show()
     }
+}
+
+function getWidth(){
+    return $('.dashboard-body').width()
 }
